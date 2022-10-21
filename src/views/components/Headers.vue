a85b3576f4375e060e9024b2db218fa7835d2b66
Taras Yemtsov <hardmagnett@yandex.ru>
2022-10-22 00:51:13 +0300

    IBDM 02

--- src/views/components/Headers.vue
+++ src/views/components/Headers.vue
@@ -1,6 +1,38 @@
 <template>
   <div class="headers">
-    <p>Заголовки</p>
+    <div class="container">
+
+      <h1>Заголовок h1</h1>
+      <h2>Заголовок h2</h2>
+      <h3>Заголовок h3</h3>
+      <h4>Заголовок h4</h4>
+      <h5>Заголовок h5</h5>
+      <h6>Заголовок h6</h6>
+      <br>
+      <br>
+      <p class="h1">Параграф .h1</p>
+      <p class="h2">Параграф .h2</p>
+      <p class="h3">Параграф .h3</p>
+      <p class="h4">Параграф .h4</p>
+      <p class="h5">Параграф .h5</p>
+      <p class="h6">Параграф .h6</p>
+      <br>
+      <br>
+      <h1 class="h1-base">Заголовок h1.h1-base</h1>
+      <h2 class="h2-base">Заголовок h2.h2-base</h2>
+      <h3 class="h3-base">Заголовок h3.h3-base</h3>
+      <h4 class="h4-base">Заголовок h4.h4-base</h4>
+      <h5 class="h5-base">Заголовок h5.h5-base</h5>
+      <h6 class="h6-base">Заголовок h6.h6-base</h6>
+      <br>
+      <br>
+      <p class="h1-base">Параграф .h1-base</p>
+      <p class="h2-base">Параграф .h2-base</p>
+      <p class="h3-base">Параграф .h3-base</p>
+      <p class="h4-base">Параграф .h4-base</p>
+      <p class="h5-base">Параграф .h5-base</p>
+      <p class="h6-base">Параграф .h6-base</p>
+    </div>
   </div>
 </template>
 
