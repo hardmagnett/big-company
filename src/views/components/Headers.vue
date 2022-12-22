1335d277ba4e1f54731df55bea00357523fa2f28
Taras Yemtsov <hardmagnett@yandex.ru>
2022-12-23 00:10:37 +0300

    Рефакторинг стилей.

--- src/views/components/Headers.vue
+++ src/views/components/Headers.vue
@@ -16,22 +16,6 @@
       <p class="h6">Параграф .h6</p>
       <br>
       <br>
-      <h1 class="h1-base">Заголовок h1.h1-base</h1>
-      <h2 class="h2-base">Заголовок h2.h2-base</h2>
-      <h3 class="h3-base">Заголовок h3.h3-base</h3>
-      <h4 class="h4-base">Заголовок h4.h4-base</h4>
-      <h5 class="h5-base">Заголовок h5.h5-base</h5>
-      <h6 class="h6-base">Заголовок h6.h6-base</h6>
-      <br>
-      <br>
-      <p class="h1-base">Параграф .h1-base</p>
-      <p class="h2-base">Параграф .h2-base</p>
-      <p class="h3-base">Параграф .h3-base</p>
-      <p class="h4-base">Параграф .h4-base</p>
-      <p class="h5-base">Параграф .h5-base</p>
-      <p class="h6-base">Параграф .h6-base</p>
-
-      <br>
       <p>Lorem ipsum dolor <a href="http://ya.ru">ya.ru</a> sit amet, consectetur adipisicing elit. Consectetur doloribus eum, laudantium natus nobis non rem. Corporis ipsum itaque officia quibusdam recusandae, sed sint? Dolor nobis non provident suscipit totam.</p>
       <br>
       <br>
