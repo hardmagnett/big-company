baaf25fce8c54bf2c40ebf650a0d746e44536b98
Taras Yemtsov <hardmagnett@yandex.ru>
2022-12-24 22:31:15 +0300

    Иконки 02

--- src/a-library/components/layout/AHeader/AHeader.vue
+++ src/a-library/components/layout/AHeader/AHeader.vue
@@ -1,42 +1,20 @@
-<!--<script>-->
-
-<!--export default {-->
-<!--  props: {-->
-<!--    // items: {-->
-<!--    //   type: Array,-->
-<!--    //   default: () => [-->
-<!--    //     { to: { name: 'components' }, text: 'Components' },-->
-<!--    //   ]-->
-<!--    // }-->
-<!--  },-->
-
-<!--}-->
-<!--</script>-->
-
-
 <script setup lang="ts">
-// todo:: По факту это не меню должно-быть, а просто хедер со слотами. Переделать
+// todo:: Логотип и текст возле лого должны быть параметрами
 
 </script>
 
 <template>
-  <div class="header-menu">
-    <!--<img alt="Логотип" src="@/assets/app/images/logo.svg" width="24" height="24"-->
-    <img alt="Логотип" src="@/app/assets/images/logo.svg" width="24" height="24"
-    />
-    <span class="color-test">trololo</span>
-    <!--<container>-->
-
-    <!--</container>-->
+  <div class="a-header">
+    <img alt="Логотип" src="@/app/assets/images/logo.svg" height="28"/>
+    <span class="a-header__text-near-logo">CRM</span>
   </div>
 </template>
 
 
 
 <style scoped lang="scss">
-  .header-menu {
+  .a-header {
     height: var(--headerHeight);
-    //background-color: clrFill();
     background-color: var(--clrFillBlueSmall);
     position: fixed;
     top: 0;
@@ -44,38 +22,12 @@
     display: flex;
     flex-flow: row nowrap;
 
-    // эти 3 - просто чтобы верхушка не смотрелась уродливо.
     align-items: center;
-    gap: 16px;
-    padding-left: 16px;
+    gap: var(--gap);
+    padding-left: var(--gap);
 
-    .color-test {
+    .a-header__text-near-logo {
       color: var(--clrFontBlueLight);
     }
-
-    // Скопировал. Нужно опробовать.
-    //color: clrFont(light, main);
-
-    //.container {
-    //  height: 100%;
-    //  display: flex;
-    //  flex-flow: row nowrap;
-    //}
-
-    //&__link {
-    //  color: clrFont(light, main);
-    //  height: 100%;
-    //  padding: 0 $gap;
-    //  line-height: $headerHeight;
-    //  text-decoration: none;
-    //  transition: background-color $timeShort;
-    //  &:first-child {
-    //    margin-left: -$gap;
-    //  }
-    //  &--active,
-    //  &:hover {
-    //    background-color: clrFill(accent);
-    //  }
-    //}
   }
 </style>
