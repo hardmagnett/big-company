c9dae162a70bbbefc1fdd82d21ff9b741b792a85
Taras Yemtsov <hardmagnett@yandex.ru>
2024-05-20 18:04:26 +0300

    Переменные для прекпоинтов 1

--- src/app/components/employees/EmployeesTable/EmployeesTable.vue
+++ src/app/components/employees/EmployeesTable/EmployeesTable.vue
@@ -26,39 +26,18 @@ import EmployeeRow from '@/app/components/employees/EmployeeRow/EmployeeRow.vue'
       //flex: 0 1 100%;
       //flex: 0 0 auto;
       --flex-items-qty: 1;
-      @media (min-width: $bpSm) {
-        --flex-items-qty: 2;
-        //@include flexItemBasis(2);
-        //flex-basis: 100%;
+      @container style(--pb-sx-or-more) {
+        --flex-items-qty: 1;
       }
-
-      @media (min-width: $bpMd) {
+      @container style(--bp-md-or-more) {
         --flex-items-qty: 2;
-        //@include flexItemBasis(2);
-        //flex-basis: 50%;
-        //max-width: 50%;
-        //outline: 1px solid darkred;
       }
-      @media (min-width: $bpLg) {
-        //flex-basis: 50%;
-        //calc()
-        //@include flexItemBasis(3);
+      @container style(--bp-lg-or-more) {
         --flex-items-qty: 3;
-        //flex-basis: 33%;
       }
-
-      @media (min-width: $bpXl) {
-        //@include flexItemBasis(4);
+      @container style(--bp-xl-or-more) {
         --flex-items-qty: 4;
-        //flex-basis:  25%;
-
-
-
-        //flex-basis: 100px;
-        //@include flexItemBasis(3, var(--gap));
-        //@include flexItemWidthAndMargin(4, $gap);
       }
-
     }
   }
 </style>
