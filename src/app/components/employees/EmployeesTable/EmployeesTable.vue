e77a3105a2bcb5a292fa28d102cc1e3b1284eda3
Taras Yemtsov <hardmagnett@yandex.ru>
2024-05-20 18:13:10 +0300

    Minor

--- src/app/components/employees/EmployeesTable/EmployeesTable.vue
+++ src/app/components/employees/EmployeesTable/EmployeesTable.vue
@@ -13,18 +13,19 @@ import EmployeeRow from '@/app/components/employees/EmployeeRow/EmployeeRow.vue'
 
 </script>
 
+<!--<style scoped>-->
+<!--<style scoped lang="css">-->
 <style scoped lang="scss">
+
 
   .employees-table {
     display: flex;
     flex-flow: row wrap;
     gap: var(--gap);
-    //gap: $gap;
-    //gap: 0;
+    /* gap: $gap; */
+    /* gap: 0; */
 
     .employee-row {
-      //flex: 0 1 100%;
-      //flex: 0 0 auto;
       --flex-items-qty: 1;
       @container style(--pb-sx-or-more) {
         --flex-items-qty: 1;
