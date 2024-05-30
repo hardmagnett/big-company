4132ff17b6e8a58f3c1512876c83ec3055e563a5
Taras Yemtsov <hardmagnett@yandex.ru>
2024-05-30 12:52:55 +0300

    Отключение анимаций

--- src/a-library/stores/responsive.js
+++ src/a-library/stores/responsive.js
@@ -2,12 +2,38 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import getCSSVariable from "@/a-library/helpers/DOM/getCSSVariable"
 
+/**
+ * Вообще хранилище не должно знать ничего о DOM.
+ * Здесь, в порядке исключения, хранилище знает о DOM,
+ * потому-что данные об адаптивном состоянии в реактивном виде могут понадобиться любому из компонентов.
+ * Возможно в будущем перенесу это куда-нибудь. А пока-что пусть лежит здесь.
+ */
+
+function stopTransitionsOnDocumentResize(){
+  const bodyClasses = document.body.classList;
+  const classToToggle = 'mod--stop-transitions'
+  if (stopTransitionsOnDocumentResize.timer) {
+    clearTimeout(stopTransitionsOnDocumentResize.timer);
+    stopTransitionsOnDocumentResize.timer = null;
+  }
+  else {
+    bodyClasses.add(classToToggle);
+  }
+  stopTransitionsOnDocumentResize.timer = setTimeout(() => {
+    bodyClasses.remove(classToToggle);
+    stopTransitionsOnDocumentResize.timer = null;
+  }, 100);
+}
+stopTransitionsOnDocumentResize.timer = null
+
 export const useResponsiveStore = defineStore('responsive', ()=>{
   let documentElement = document.documentElement
 
   let documentWidth = ref(documentElement.clientWidth)
 
   window.addEventListener('resize', ()=>{
+    stopTransitionsOnDocumentResize()
+
     let newVal = documentElement.clientWidth
     documentWidth.value = newVal
   });
