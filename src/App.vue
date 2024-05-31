66fc860417ee3d100be291b8a6c360b57171e6bd
Taras Yemtsov <hardmagnett@yandex.ru>
2024-05-31 16:13:27 +0300

    Остались a-library, app, и storage

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import AHamburger from "@/a-library/components/layout/AHamburger/AHamburger.vue";
 
 </script>
 
 <template>
   <div class="app">
-    <!--<AHamburger />-->
-
-
-
-    <!--<AMainMenu/>-->
-
     <AContainer>
-      <!--<template v-slot:header>-->
-      <!--  <APageHeader>Структура кампании</APageHeader>-->
-      <!--</template>-->
       <RouterView />
     </AContainer>
   </div>
@@ -23,12 +13,6 @@ import AHamburger from "@/a-library/components/layout/AHamburger/AHamburger.vue"
 </template>
 
 <style scoped>
-/*.a-hamburger {*/
-/*  position: absolute;*/
-/*  top: 130px;*/
-/*  left: 130px;*/
-/*  z-index: 15;*/
-/*}*/
 .app {
   display: flex;
   width: 100%;
@@ -36,12 +20,10 @@ import AHamburger from "@/a-library/components/layout/AHamburger/AHamburger.vue"
 
   height: 100%;
   position: absolute;
-  .main-menu {
-    flex: 0 0 auto;
-  }
+
   .a-container {
     flex: 1 1 auto;
-    overflow-y: auto;
+    overflow-y: auto; /*Не помню зачем это. Хорошо-бы узнать*/
   }
 }
 
