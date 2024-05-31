e29735be01f8fab4e23fe80c2ede8abb6dffbdc9
Taras Yemtsov <hardmagnett@yandex.ru>
2024-05-31 16:34:47 +0300

    Stores

---
+++ hints/tmplPiniaModuleTS--NoMagic.ts
@@ -0,0 +1,28 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+export const useMyShedStore = defineStore('MyShed', () => {
+  // state variable
+  const count = ref(0)
+
+  // action
+  function increment() {
+    count.value++
+  }
+
+  // getter без параметров
+  const doubleCount = computed(() => count.value * 2)
+
+  // параметрический getter
+  const multiplyCount = computed((state) => {
+    return (myMultiplier: number) => {
+      return count.value * myMultiplier
+    }
+  })
+
+  // Все публичные state variables, actions и getters должны быть экспортированы
+  return {
+    count,
+    doubleCount,
+    increment
+  }
+})
