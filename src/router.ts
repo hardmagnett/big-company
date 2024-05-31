4d47def388b6a4a8f982f9ec199b6440a2a9fbcd
Taras Yemtsov <hardmagnett@yandex.ru>
2024-05-31 18:54:58 +0300

    Minor

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import appRoutes from '@/app/router/index.ts';
-import aLibraryRoutes from '@/a-library/router/index.ts';
+import appRoutes from '@/app/router/index';
+import aLibraryRoutes from '@/a-library/router/index';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
