c6685e322f3169d265673d526f6980ac57b2a3d8
Taras Yemtsov <hardmagnett@yandex.ru>
2024-06-12 16:02:51 +0300

    Таблица в сотрудниках 02

--- src/a-library/components/other/ATable/ATable.vue
+++ src/a-library/components/other/ATable/ATable.vue
@@ -10,11 +10,18 @@
   </div>
 </template>
 
-<!--todo:: вернуть scoped-->
-<style scoped>
+<!--Здесь стили глобальные. Не scoped. Почему?-->
+<!--Потому-что нужно стилизовать контент, который передается в слот. Всякие tr и td-->
+<!--В простом случае, когда tr и td передаются напрямую, спасал :slotted.-->
+<!--Но если в слот передавать какой-то другой компонент, уже внутри которого будут tr и td, -->
+<!--то тогда :slotted не работает и строки таблицы остаются нестилизованными-->
+<!--Поэтому пусть здесь будет без scoped. К тому-же эти стили и так ограничены классом-родителем .a-table,-->
+<!--Поэтому ничего плохого не случится-->
+<style>
 .a-table {
   overflow-y: auto;
 
+  /*&::v-deep(a-table) {*/
   table {
     width: 100%;
 
@@ -30,12 +37,15 @@
     border-spacing: 0;
     border-collapse: collapse;
 
-    :slotted(tr) {
+    /*:slotted(tr) {*/
+    tr {
       background-color: var(--background-color);
-      :slotted(th){
+      /*:slotted(th){*/
+      th {
         font-weight: var(--font-weight-bold);
       }
-      :slotted(th), :slotted(td) {
+      /*:slotted(th), :slotted(td) {*/
+      th, td {
         padding: calc(var(--gap) / 2) var(--gap);
         text-align: left;
         border-bottom: 1px solid var(--border-color);
@@ -43,14 +53,16 @@
       }
     }
 
-    :slotted(:not(thead) tr:hover) {
+    /*:slotted(:not(thead) tr:hover) {*/
+    :not(thead) tr:hover {
       background-color: var(--clr-bg-blue-small);
     }
   }
 
   &.a-table--fixed-header {
     table {
-      :slotted(thead) {
+      /*:slotted(thead) {*/
+      thead {
         /*background-color: #aff;*/
 
         position: sticky;
@@ -59,7 +71,8 @@
 
         /*Хак. С таблицами нередко необычное приходится делать.*/
         /*Нормальными способами у таблицы бордюр ускролливается.*/
-        :slotted(&::before) {
+        /*:slotted(&::before) {*/
+        &::before {
           content: '';
           position: absolute;
           bottom: 0;
