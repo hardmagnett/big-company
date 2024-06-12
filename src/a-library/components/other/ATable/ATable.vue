8774c0ca97ebcddd08c57daa9c145a2fc469c5f1
Taras Yemtsov <hardmagnett@yandex.ru>
2024-06-12 10:55:03 +0300

    Таблица в процессе

--- src/a-library/components/other/ATable/ATable.vue
+++ src/a-library/components/other/ATable/ATable.vue
@@ -11,6 +11,25 @@
 
 <style scoped>
 .a-table {
-  outline: 1px solid darkred;
+  width: 100%;
+
+  /*Чтобы убрать всякие отступы между ячейками*/
+  border-spacing: 0;
+  border-collapse: collapse;
+
+  :slotted(tr) {
+    /*border-top: 1px solid green;*/
+    /*border-left: 1px solid red;*/
+    /*border-spacing: 0;*/
+    /*background-color: red;*/
+    /*:slotted(td, th) {*/
+    :slotted(th), :slotted(td) {
+      /*border-spacing: 0;*/
+      padding: var(--gap);
+      text-align: left;
+      /*background-color: #aff;*/
+      border-bottom: 1px solid orange;
+    }
+  }
 }
 </style>
--- src/a-library/views/component-list/ViewTables/ViewTables.vue
+++ src/a-library/views/component-list/ViewTables/ViewTables.vue
@@ -7,7 +7,20 @@
     <h2>Таблицы</h2>
     <p>Продолжить отсюда</p>
     https://vuetifyjs.com/en/components/tables/#api
-    <ATable> foo </ATable>
+    <ATable>
+      <thead>
+        <tr>
+          <th>Колонка 01</th>
+          <th>Колонка 02</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="n in 10" :key="n">
+          <td>Колонка 01</td>
+          <td>Колонка 02</td>
+        </tr>
+      </tbody>
+    </ATable>
   </div>
 </template>
 
@@ -15,4 +28,5 @@
 .tables {
 }
 </style>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
