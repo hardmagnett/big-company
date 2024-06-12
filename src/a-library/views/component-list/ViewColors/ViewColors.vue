4fb41fda8766341ef7798bcdcb294c90b9154169
Taras Yemtsov <hardmagnett@yandex.ru>
2024-06-12 12:50:36 +0300

    Закончил страницу с цветами

--- src/a-library/views/component-list/ViewColors/ViewColors.vue
+++ src/a-library/views/component-list/ViewColors/ViewColors.vue
@@ -5,39 +5,39 @@ const colorNames: Array<string> = ['blue', 'red', 'green', 'orange']
 let colorBGCSSVariables: Array<[string, string, string, string, string]> = []
 for (let colorName of colorNames) {
     colorBGCSSVariables.push([
-      `--clr-bg-${colorName}-big`,
-      `--clr-bg-${colorName}-small`,
-      `--clr-bg-${colorName}-smaller`,
-      `--clr-bg-${colorName}-accent`,
       `--clr-bg-${colorName}-highlight`,
+      `--clr-bg-${colorName}-accent`,
+      `--clr-bg-${colorName}-smaller`,
+      `--clr-bg-${colorName}-small`,
+      `--clr-bg-${colorName}-big`,
     ])
 }
 
 let colorFillCSSVariables: Array<[string, string, string]> = []
 for (let colorName of colorNames) {
   colorFillCSSVariables.push([
-    `--clr-fill-${colorName}-small`,
-    `--clr-fill-${colorName}-big`,
     `--clr-fill-${colorName}-accent`,
+    `--clr-fill-${colorName}-big`,
+    `--clr-fill-${colorName}-small`,
   ])
 }
 
 let colorBorderCSSVariables: Array<[string, string, string]> = []
 for (let colorName of colorNames) {
   colorBorderCSSVariables.push([
-    `--clr-border-${colorName}-lightest`,
-    `--clr-border-${colorName}-lighter`,
     `--clr-border-${colorName}-darker`,
+    `--clr-border-${colorName}-lighter`,
+    `--clr-border-${colorName}-lightest`,
   ])
 }
 
 let colorFontCSSVariables: Array<[string, string, string, string]> = []
 for (let colorName of colorNames) {
   colorFontCSSVariables.push([
-    `--clr-font-${colorName}-light`,
-    `--clr-font-${colorName}-gray`,
-    `--clr-font-${colorName}-dark`,
     `--clr-font-${colorName}-darkest`,
+    `--clr-font-${colorName}-dark`,
+    `--clr-font-${colorName}-gray`,
+    `--clr-font-${colorName}-light`,
   ])
 }
 
@@ -53,42 +53,50 @@ for (let colorName of colorNames) {
     <h2>Фоны</h2>
     <p>Для блоков большого размера.</p>
 
-    <ATable>
-        <tr
+    <div class="view-colors__wrapper-for-bgs">
+      <template
           v-for="(colorGroup, index) in colorBGCSSVariables"
           :key="index"
-        >
-          <td
-              v-for="(variableName, index) in colorGroup" :key="index"
-              :style="{
-                backgroundColor: `var(${variableName})`
-              }"
-          >
-            {{ variableName }}
-          </td>
-        </tr>
-    </ATable>
-
-    <br>
-
-    <h2>Заполнения</h2>
-    <p>Ярко выраженный цвет. Например кнопка. Максимум - шапка.</p>
-
-    <ATable>
-      <tr
-          v-for="(colorGroup, index) in colorFillCSSVariables"
-          :key="index"
       >
-        <td
+        <div
+            class="
+              view-colors__color-block
+              mod--basis-for-column-flex-item
+            "
             v-for="(variableName, index) in colorGroup" :key="index"
             :style="{
                 backgroundColor: `var(${variableName})`
               }"
         >
           {{ variableName }}
-        </td>
-      </tr>
-    </ATable>
+        </div>
+      </template>
+    </div>
+
+    <br>
+
+    <h2>Заполнения</h2>
+    <p>Ярко выраженный цвет. Например кнопка. Максимум - шапка.</p>
+
+    <div class="view-colors__wrapper-for-fills">
+      <template
+          v-for="(colorGroup, index) in colorFillCSSVariables"
+          :key="index"
+      >
+        <div
+            class="
+              view-colors__color-block
+              mod--basis-for-column-flex-item
+            "
+            v-for="(variableName, index) in colorGroup" :key="index"
+            :style="{
+                backgroundColor: `var(${variableName})`
+              }"
+        >
+          {{ variableName }}
+        </div>
+      </template>
+    </div>
     <br>
 
     <h2></h2>
@@ -116,7 +124,6 @@ for (let colorName of colorNames) {
     <br>
 
     <h2>Шрифты</h2>
-    <!--<div class="view-colors__wrapper-for-fonts">-->
       <template
           v-for="(colorGroup, index) in colorFontCSSVariables"
           :key="index"
@@ -130,39 +137,42 @@ for (let colorName of colorNames) {
           {{ variableName }}
         </p>
         <br>
-        <!--<ACard-->
-        <!--    class="mod&#45;&#45;basis-for-column-flex-item"-->
-        <!--    v-for="(variableName, index) in colorGroup" :key="index"-->
-        <!--    :style="{-->
-        <!--        color: `var(${variableName})`-->
-        <!--      }"-->
-        <!--&gt;-->
-        <!--  {{ variableName }}-->
-        <!--</ACard>-->
       </template>
-    <!--</div>-->
   </div>
 </template>
 
 <style scoped>
 .view-colors {
-  .view-colors__wrapper-for-borders {
-    --flex-items-qty: 3;
+  .view-colors__color-block {
+    padding: var(--gap)
+  }
+  .view-colors__wrapper-for-borders,
+  .view-colors__wrapper-for-fills,
+  .view-colors__wrapper-for-bgs {
     display: flex;
     flex-flow: row wrap;
+  }
+  .view-colors__wrapper-for-borders {
+    --flex-items-qty: 3;
     gap: var(--gap);
     @container style(--bp-md-or-less) {
       --flex-items-qty: 1;
     }
   }
-  /*.view-colors__wrapper-for-fonts {*/
-  /*    --flex-items-qty: 4;*/
-  /*    display: flex;*/
-  /*    flex-flow: row wrap;*/
-  /*    gap: var(--gap);*/
-  /*  @container style(--bp-md-or-less) {*/
-  /*    --flex-items-qty: 1;*/
-  /*  }*/
-  /*}*/
+  .view-colors__wrapper-for-fills {
+    --flex-items-qty: 3;
+    --gap-flex: 0px;
+    @container style(--bp-md-or-less) {
+      --flex-items-qty: 1;
+    }
+  }
+  .view-colors__wrapper-for-bgs {
+    --flex-items-qty: 5;
+    --gap-flex: 0px;
+    @container style(--bp-lg-or-less) {
+      --flex-items-qty: 1;
+    }
+  }
+
 }
 </style>
