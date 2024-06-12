e0d42636bb3cc8a51c5eb9f1d7f9201b843cd150
Taras Yemtsov <hardmagnett@yandex.ru>
2024-06-12 11:46:55 +0300

    Цвета 01

--- src/a-library/views/component-list/ViewColors/ViewColors.vue
+++ src/a-library/views/component-list/ViewColors/ViewColors.vue
@@ -1,10 +1,67 @@
+<script setup lang="ts">
+
+const colorNames: Array<string> = ['blue', 'red', 'green', 'orange']
+let colorBGCSSVariables = []
+for (let colorName of colorNames) {
+    colorBGCSSVariables.push([
+      `--clr-bg-${colorName}-big`,
+      `--clr-bg-${colorName}-small`,
+      `--clr-bg-${colorName}-smaller`,
+      `--clr-bg-${colorName}-accent`,
+      `--clr-bg-${colorName}-highlight`,
+    ])
+}
+</script>
+
 <template>
   <div class="colors">
     <Teleport to="#page-header-place">
       <APageHeader> Цвета </APageHeader>
     </Teleport>
 
-    <h2>Здесь будет про цвета</h2>
+    <h2>Фоны</h2>
+    <p>Для блоков большого размера.</p>
+
+    <ATable>
+      <thead>
+      <!--<tr>-->
+      <!--  <th>big - например фон всей страницы</th>-->
+      <!--  <th>small - например для шапки в таблице</th>-->
+      <!--  <th>accent - Чтобы выделялось на фоне любого серого</th>-->
+      <!--  <th>big - например фон всей страницы</th>-->
+      <!--</tr>-->
+      </thead>
+      <tbody>
+      <p ></p>
+
+        <tr
+          v-for="(colorGroup, index) in colorBGCSSVariables"
+          :key="index"
+        >
+          <td
+              v-for="(variableName, index) in colorGroup" :key="index"
+              style="background-color: var(--clr-bg-blue-big)"
+              :style="{
+                backgroundColor: `var(${variableName})`
+              }"
+          >
+            {{ variableName }}
+          </td>
+        </tr>
+      </tbody>
+
+    </ATable>
+
+    <h2>Заполнения</h2>
+    <p>Ярко выраженный цвет. Например кнопка. Максимум - шапка.</p>
+
+    <h2></h2>
+    <p></p>
+
+    <h2>Бордюры</h2>
+
+    <h2>Шрифты</h2>
+
   </div>
 </template>
 
@@ -12,5 +69,3 @@
 .colors {
 }
 </style>
-<script setup lang="ts">
-</script>
