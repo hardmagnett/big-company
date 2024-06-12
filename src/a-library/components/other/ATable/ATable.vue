df9c3c1f085085d548cfaaa41e382838bf8200a9
Taras Yemtsov <hardmagnett@yandex.ru>
2024-06-12 14:34:10 +0300

    Таблицы

--- src/a-library/components/other/ATable/ATable.vue
+++ src/a-library/components/other/ATable/ATable.vue
@@ -17,20 +17,28 @@
 
   table {
     width: 100%;
-    
+
+    /*Нужна чтобы цветом перекрывать задний фон при фиксированной шапке.*/
+    --background-color: var(--clr-bg-blue-big);
+
+    --border-color: var(--clr-border-blue-lighter);
+
+
+    background-color: var(--background-color);
 
     /*Чтобы убрать всякие отступы между ячейками*/
     border-spacing: 0;
     border-collapse: collapse;
 
     :slotted(tr) {
+      background-color: var(--background-color);
       :slotted(th){
         font-weight: var(--font-weight-bold);
       }
       :slotted(th), :slotted(td) {
         padding: calc(var(--gap) / 2) var(--gap);
         text-align: left;
-        border-bottom: 1px solid var(--clr-border-blue-lighter);
+        border-bottom: 1px solid var(--border-color);
         font-size: var(--font-size-small)
       }
     }
@@ -40,7 +48,6 @@
   }
 
   &.a-table--fixed-header {
-    outline: 1px solid darkred;
     table {
       :slotted(thead) {
         /*background-color: #aff;*/
@@ -48,6 +55,18 @@
         position: sticky;
         top: 0;
         z-index: 2;
+
+        /*Хак. С таблицами нередко необычное приходится делать.*/
+        /*Нормальными способами у таблицы бордюр ускролливается.*/
+        :slotted(&::before) {
+          content: '';
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          right: 0;
+          height: 1px;
+          background-color: var(--border-color);
+        }
       }
     }
   }
