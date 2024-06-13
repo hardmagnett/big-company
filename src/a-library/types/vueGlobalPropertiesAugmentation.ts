7c627cc514a2ed6835c40f66a1cfa1036465122e
Taras Yemtsov <hardmagnett@yandex.ru>
2024-06-13 19:01:06 +0300

    TYPE_CHECK

---
+++ src/a-library/types/vueGlobalPropertiesAugmentation.ts
@@ -0,0 +1,25 @@
+/**
+ * Этот файл нужен, чтобы добавлять общедоступные свойства во все компоненты.
+ * Например, при помощи vue-плагинов.
+ *
+ * В документации написано было что этот файл нужно подключить в tsconfig.
+ * https://vuejs.org/guide/typescript/options-api.html#augmenting-global-properties
+ * Удивительно, но я этот файл вообще никуда не подключал,
+ * и vue-tsc проходит без ошибок.
+ * Возможно возникнут проблемы, когда я сделаю ещё одно расширение для globalProperties.
+ */
+
+import {useResponsiveStore} from "@/a-library/stores/responsive";
+
+const valueWithTypeOfUseResponsiveStore = useResponsiveStore()
+declare module 'vue' {
+  interface ComponentCustomProperties {
+    $responsiveVariables: typeof valueWithTypeOfUseResponsiveStore
+  }
+}
+
+/**
+ * Так-же в документации было написано, что обязательно нужно сделать пустой экспорт.
+ * Но у меня заработало без пустого экспорта.
+ */
+// export {}
