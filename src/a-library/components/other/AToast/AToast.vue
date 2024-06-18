12d94c8c4f6de20dc98fd59dd1e34bcb55aa4c00
Taras Yemtsov <hardmagnett@yandex.ru>
2024-06-18 12:35:51 +0300

    toaster 02 - Остановился на defineProps

---
+++ src/a-library/components/other/AToast/AToast.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { computed, onMounted, ref } from "vue";
+
+// Props for our component,
+// these are the same as Notitfication interface.
+const props = defineProps({
+  id: { type: String, required: true },
+  type: {
+    type: String,
+    default: "info",
+    required: false,
+  },
+  title: { type: String, default: null, required: false },
+});
+
+</script>
+
+<template>
+  <div class="a-toast">
+  </div>
+</template>
+
+<style scoped lang="scss">
+.a-toast {}
+</style>
---
+++ src/a-library/components/other/AToast/useAToast.ts
@@ -0,0 +1,52 @@
+import createUUID from '@/a-library/helpers/language/createUUID';
+
+import { ref } from "vue";
+
+export interface Notification {
+  id: string;
+  type: string;
+  message: string;
+}
+
+// todo:: заменить интерфейс на тот, который я сделал. Мне мой больше нравится.
+export type CreateNotification = {
+  (options: {
+    type?: string;
+    message?: string;
+  }): void;
+};
+
+// todo:: упростить. Постараться избавиться от этого default options заменой на дефолтовые параметры CreateNotification
+const defaultNotificationOptions = {
+  type: "info",
+  message:
+    "Ooops! A message was not provided.",
+};
+
+export default function useAToast() {
+  const notifications = ref<Notification[]>([])
+  // function to create notification
+  const createNotification: CreateNotification = (options) => {
+    const _options = Object.assign({ ...defaultNotificationOptions }, options);
+    notifications.value.push(
+      // todo:: вот эта конструкция какая-то кривая. Упростить.
+      ...[
+        {
+          id: createUUID(),
+          ..._options,
+        },
+      ]
+    )
+  }
+  const removeNotifications = (id: string) => {
+    const index = notifications.value.findIndex((item) => item.id === id);
+    if (index !== -1) notifications.value.splice(index, 1);
+  };
+
+  return {
+    notifications,
+    createNotification,
+    removeNotifications,
+  };
+
+}
