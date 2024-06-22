1a12b63ea93daa7c329f3cabcd58f3632d8c57f3
Taras Yemtsov <hardmagnett@yandex.ru>
2024-06-22 18:28:59 +0300

    Сетка 02

--- src/a-library/views/component-list/ViewColumnGrid/ViewColumnGrid.vue
+++ src/a-library/views/component-list/ViewColumnGrid/ViewColumnGrid.vue
@@ -9,41 +9,49 @@
     </p>
     <p>Например потаскать браузер за правый или левый край.</p>
 
-    <div class="column-grid__grid-container">
+    <div class="am-cols">
       <ACard
-        class="column-grid__grid-item mod--basis-for-column-flex-item"
+        class="am-col-12 am-col-sm-6 am-col-md-4 am-col-lg-3 am-col-xl-2 am-col-xxl-1"
         :key="n"
         v-for="n in 60"
       >
-        Элемент сетки
+        # {{ n }}
       </ACard>
+
+      <!--<div-->
+      <!--    class="am-col-12 am-col-sm-6 am-col-md-4 am-col-lg-3 am-col-xl-2 am-col-xxl-1"-->
+      <!--    v-for="n in 36"-->
+      <!--    :key="n"-->
+      <!--&gt;: {{ n }}</div>-->
     </div>
   </div>
 </template>
 
 <style scoped>
 .column-grid {
-  .column-grid__grid-container {
-    display: flex;
-    flex-flow: row wrap;
-    gap: var(--gap);
-    .column-grid__grid-item {
-      --flex-items-qty: 1;
-      height: 100px;
+  /*.column-grid__grid-container {*/
+  /*  display: flex;*/
+  /*  flex-flow: row wrap;*/
+  /*  gap: var(--gap);*/
+  /*  .column-grid__grid-item {*/
+  /*    --flex-items-qty: 1;*/
+  /*    height: 100px;*/
 
-      @container style(--pb-sx-or-more) {
-        --flex-items-qty: 1;
-      }
-      @container style(--bp-md-or-more) {
-        --flex-items-qty: 2;
-      }
-      @container style(--bp-lg-or-more) {
-        --flex-items-qty: 3;
-      }
-      @container style(--bp-xl-or-more) {
-        --flex-items-qty: 4;
-      }
-    }
-  }
+  /*    @container style(--pb-sx-or-more) {*/
+  /*      --flex-items-qty: 1;*/
+  /*    }*/
+  /*    @container style(--bp-md-or-more) {*/
+  /*      --flex-items-qty: 2;*/
+  /*    }*/
+  /*    @container style(--bp-lg-or-more) {*/
+  /*      --flex-items-qty: 3;*/
+  /*    }*/
+  /*    @container style(--bp-xl-or-more) {*/
+  /*      --flex-items-qty: 4;*/
+  /*    }*/
+  /*  }*/
+  /*}*/
 }
 </style>
+<script setup lang="ts">
+</script>
--- src/a-library/views/component-list/ViewForm/ViewForm.vue
+++ src/a-library/views/component-list/ViewForm/ViewForm.vue
@@ -3,48 +3,44 @@
     <Teleport to="#page-header-place">
       <APageHeader> Форма </APageHeader>
     </Teleport>
-    <p>В форме работает навигация по элементам при помощи клавиши <code class="mod--code">tab</code>.</p>
+    <p>
+      В форме работает навигация по элементам при помощи клавиши
+      <code class="mod--code">tab</code>.
+    </p>
 
-    <div class="
-      am-cols
-      view-form__inputs
-    ">
+    <div class="am-cols view-form__inputs">
+      <!--<div-->
+      <!--    class="am-col-12 am-col-sm-6 am-col-md-4 am-col-lg-3 am-col-xl-2 am-col-xxl-1"-->
+      <!--    v-for="n in 36"-->
+      <!--    :key="n"-->
+      <!--&gt;: {{ n }}</div>-->
+      <!--<div class="am-col-3">1</div>-->
+      <!--<div class="am-col-6">2</div>-->
+      <!--<div class="am-col-3">3</div>-->
+      <!--<div class="am-col-1">4</div>-->
+      <!--<div class="am-col-1">5</div>-->
+      <!--<div class="am-col-2">6</div>-->
+      <!--<div class="am-col-3">7</div>-->
+      <!--<div class="am-col-5">8</div>-->
+      <!--<div class="am-col-3">9</div>-->
+      <!--<div class="am-col-3">10</div>-->
 
-      <div class="am-col-3">1</div>
-      <div class="am-col-6">2</div>
-      <div class="am-col-3">3</div>
-      <div class="am-col-1">4</div>
-      <div class="am-col-1">5</div>
-      <div class="am-col-2">6</div>
-      <div class="am-col-3">7</div>
-      <div class="am-col-5">8</div>
-      <div class="am-col-3">9</div>
-      <div class="am-col-3">10</div>
-      <!--<AInput-->
-      <!--    class="-->
-      <!--      mod&#45;&#45;flex-columns-item-->
-      <!--      mod&#45;&#45;basis-for-column-flex-itemZ-->
-      <!--    "-->
-      <!--    label="Текстовое поле"-->
-      <!--    hideHint-->
-      <!--&gt;</AInput>-->
+      <AInput
+          class="am-col-12 am-col-sm-6"
+          label="Текстовое поле"
+          hideHint
+      ></AInput>
 
-      <!--<ACheckBox-->
-      <!--    class="-->
-      <!--      mod&#45;&#45;flex-columns-item-->
-      <!--      mod&#45;&#45;basis-for-column-flex-itemZ-->
-      <!--    "-->
+      <ACheckBox
+          class="am-col-12 am-col-sm-6"
 
-      <!--    label="Чекбокс-поле"  hide-hint/>-->
+          label="Чекбокс-поле"  hide-hint/>
     </div>
-
-
   </div>
 </template>
 
 <style scoped>
 .view-form {
-
   .view-form__inputs {
     /*outline: 1px solid green;*/
     /*display: flex;*/
@@ -58,5 +54,4 @@
   }
 }
 </style>
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
