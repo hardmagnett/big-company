6091f054ee3f7ede0b6d5be62980507a85c75694
Taras Yemtsov <hardmagnett@yandex.ru>
2024-06-26 14:46:27 +0300

    minor

--- src/a-library/vue-plugins/aValidator/aValidatorPlugin.ts
+++ src/a-library/vue-plugins/aValidator/aValidatorPlugin.ts
@@ -14,18 +14,8 @@ export default {
   }
 };
 
-
-// todo:: раскомментить потом в процессе работы
-// todo:: почему field_errors через snake_case, в то время пока остальные через pascalCase?
-
-// todo:: продолжить отсюда Feature 2: It should validate all input fields in document.
-
-// export const fieldErrors = AValitator.field_errors;
-// export const validateDirective = AValitator.validateDirective;
-
 // export const validateInputs = AValitator.validateInputs;
 // export const validateInput = AValitator.validateInput;
-// export const addField = AValitator.addField;
 
 // todo:: в статье написано что их можно применять вот так в компоненте напрямую.
 // Когда доделаю - выбрать как лучше и зачем они вообще здесь.
