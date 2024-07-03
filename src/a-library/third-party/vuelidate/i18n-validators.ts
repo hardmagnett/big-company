4cab3cdfdf5bd6afab945b7d85322bd611d471f9
Taras Yemtsov <hardmagnett@yandex.ru>
2024-07-03 23:58:24 +0300

    Начал делать по их совету в лоб.

--- src/a-library/third-party/vuelidate/i18n-validators.ts
+++ src/a-library/third-party/vuelidate/i18n-validators.ts
@@ -2,32 +2,18 @@
 import * as validators from '@vuelidate/validators'
 // import { required, helpers } from "@vuelidate/validators";
 import {t} from 'i18next';
-
+import {helpers} from "@vuelidate/validators";
 
 const { createI18nMessage } = validators
-const createI18nextMessage = ()=>{
-  return function withI18nextMessage(validator) {
-    return validators.helpers.withMessage(message, validator(...arguments));
-  };
-}
+// const withI18nMessage = createI18nMessage({ t: i18n.global.t.bind(i18n) })
+const withI18nMessage = createI18nMessage({ t: t })
 
 
-// const withI18nMessage = createI18nMessage({ t: I18n.global.t.bind(i18n) })
-const withI18nextMessage = createI18nextMessage(t)
+export const required = withI18nMessage(validators.required)
+export const minLength = withI18nMessage(validators.minLength, { withArguments: true })
 
-
-// export const requiredOLD = withI18nMessage(validators.required)
-// export const requiredNew = withI18nextMessage(validators.required)
-// export const minLengthNew = withI18nextMessage(validators.minLength, { withArguments: true })
-
-
-// export const minLength = withI18nMessage(validators.minLength, { withArguments: true })
-// export const maxLength = withI18nMessage(validators.maxLength(10))
-
-export const required = validators.required
-export const minLength = validators.minLength
-//
-// export  minLength
+// export const required = validators.required
+// export const minLength = validators.minLength
 
 
 
@@ -37,3 +23,5 @@ console.log(result); console.log('^...result:')
 
 result = t('validationRules.minLength', { property: 'Имя', model: 2, min: 6 });
 console.log(result); console.log('^...result:')
+
+// console.dir(t); console.log('^...t:')
