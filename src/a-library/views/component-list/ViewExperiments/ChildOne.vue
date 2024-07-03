e236e27c1c112b4cddd63399e4bb60230f40d0b6
Taras Yemtsov <hardmagnett@yandex.ru>
2024-07-03 08:25:35 +0300

    Заготовка для Экспериментов

---
+++ src/a-library/views/component-list/ViewExperiments/ChildOne.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { } from 'vue'
+
+</script>
+
+<template>
+  <div class="child-one">
+    <p>ChildOne</p>
+  </div>
+</template>
+
+<style scoped>
+.child-one {}
+</style>
---
+++ src/a-library/views/component-list/ViewExperiments/ViewExperiments.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { } from 'vue'
+import ChildOne from "@/a-library/views/component-list/ViewExperiments/ChildOne.vue";
+
+</script>
+
+<template>
+  <div class="view-experiments">
+    <Teleport to="#page-header-place">
+      <APageHeader> Эксперименты </APageHeader>
+    </Teleport>
+
+    <ChildOne />
+
+  </div>
+</template>
+
+<style scoped>
+.view-experiments {}
+</style>
