221897dc4c4af69300a3db8f91f93f41fc5e1130
Taras Yemtsov <hardmagnett@yandex.ru>
2024-07-09 13:01:03 +0300

    АПИ-интерфейс

--- src/a-library/components/forms/AMultiselect/AMultiSelect.vue
+++ src/a-library/components/forms/AMultiselect/AMultiSelect.vue
@@ -1,5 +1,30 @@
 <script setup lang="ts">
 import { } from 'vue'
+import {
+  iAInputableEmits,
+  iAInputablePropDefaults,
+} from "@/a-library/components/forms/mixins/AInputable/IAInputable";
+import type { IAInputableProps } from "@/a-library/components/forms/mixins/AInputable/IAInputable";
+
+export interface Props extends IAInputableProps {
+  options: string[] // todo:: должен так-же принимать массив обьектов
+  optionObjectFieldTitle?: string,
+  optionObjectFieldValue?: string,
+  returnObject?: boolean
+  hideSearch?: boolean
+  multiple?: boolean
+}
+withDefaults(defineProps<Props>(), {
+  ...iAInputablePropDefaults,
+  options: ()=>[],
+  optionObjectFieldTitle: 'title',
+  optionObjectFieldValue: 'id',
+  returnObject: false,
+  hideSearch: false,
+  multiple: false
+
+});
+
 
 </script>
 
