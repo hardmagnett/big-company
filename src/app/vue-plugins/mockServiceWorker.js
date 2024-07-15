da18f45184609b5c5d92ba64eb4d6b9882d90370
Taras Yemtsov <hardmagnett@yandex.ru>
2024-07-15 18:54:19 +0300

    установка msw 02

--- src/app/vue-plugins/mockServiceWorker.js
+++ src/app/vue-plugins/mockServiceWorker.js
@@ -1,13 +1,15 @@
-import { setupServer } from 'msw/node'
+import { setupWorker } from 'msw/browser'
 import { handlers } from '@/backend-mocking/handlers'
 
-export const server = setupServer(...handlers)
+// export const worker = setupWorker(...handlers)
 
-server.listen()
+// worker.listen()
 
 export default {
   // install: (app: App) => {
   install: () => {
-    server.listen()
+    // worker.listen()
+    let worker = setupWorker(...handlers)
+    worker.start()
   },
 };
