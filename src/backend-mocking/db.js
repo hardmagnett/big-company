2818bd31aca01f6fe60cb92b44ffc2e8d4b23b75
Taras Yemtsov <hardmagnett@yandex.ru>
2024-07-16 15:20:11 +0300

    DB 03

--- src/backend-mocking/db.js
+++ src/backend-mocking/db.js
@@ -1,8 +1,9 @@
 import {factory, manyOf, oneOf, primaryKey} from '@mswjs/data'
-import fable from '@/a-library/helpers/content/fable'
-import {randomBetween} from "@/a-library/helpers/language/number/randoms";
+import {seedPositions} from '@/backend-mocking/db-seeding/position'
+import {seedEmployees} from '@/backend-mocking/db-seeding/employee'
 
-export const db = factory({
+
+export const dbInstance = factory({
   position: {
     id: primaryKey(Number),
     title: String,
@@ -16,41 +17,9 @@ export const db = factory({
   }
 })
 
+seedPositions({dbInstance})
+seedEmployees({dbInstance})
 
-let positionsRaw = [
-  { id: 1, title: "IT-Менеджер" },
-  { id: 2, title: "Аналитик данных" },
-  { id: 3, title: "Backend-разработчик" },
-  { id: 4, title: "Frontend-разработчик" },
-  { id: 5, title: "IOS-разработчик" },
-  { id: 6, title: "Android-разработчик" },
-  { id: 7, title: "Программист 1C" },
-  { id: 8, title: "UI и UX дизайнер" },
-  { id: 9, title: "Администратор безопасности" },
-  { id: 10, title: "DevOps-инженер" },
-  { id: 11, title: "Специалист техподдержки" },
-  { id: 12, title: "Тестировщик" },
-  { id: 13, title: "Дата-инженер " },
-  { id: 14, title: "Аналитик данных" },
-  { id: 15, title: "Системный аналитик" },
-]
-let positions = []
-for (let positionRaw of positionsRaw) {
-  positions.push (db.position.create(positionRaw))
-} 
-
-for (let id = 1; id <= 200; id++) {
-  let gender = randomBetween(0, 1, {sameRandom: true}) === 1 ? 'female' : 'male'
-  
-  let positionIndex = randomBetween(0, positions.length - 1, {sameRandom: true})
-  let positionForEmployee = positions[positionIndex]
-  db.employee.create({ 
-    id: id, 
-    firstname: fable.firstName({gender}), 
-    lastname: fable.lastName({gender}), 
-    position: positionForEmployee
-  })
-}
 
 
 
--- src/backend-mocking/handlers.js
+++ src/backend-mocking/handlers.js
@@ -1,7 +1,7 @@
 // src/mocks/handlers.js
 import { http, HttpResponse, passthrough } from 'msw'
 
-import {db} from '@/backend-mocking/db'
+import {dbInstance} from '@/backend-mocking/db'
 
 
 let baseUrl = '/api'
@@ -9,7 +9,7 @@ let baseUrl = '/api'
 export const handlers = [
   http.get(`${baseUrl}/positions`, () => {
 
-    const allPositions = db.position.getAll()
+    const allPositions = dbInstance.position.getAll()
 
     return HttpResponse.json({
       data: allPositions.map(p=>{return {
@@ -21,7 +21,7 @@ export const handlers = [
 
   http.get(`${baseUrl}/employees`, () => {
 
-    const allEmployees = db.employee.getAll()
+    const allEmployees = dbInstance.employee.getAll()
     
 
     return HttpResponse.json({
