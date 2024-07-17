a47e160bf4cd281471a742e9383dad1404bcf00a
Taras Yemtsov <hardmagnett@yandex.ru>
2024-07-17 14:41:12 +0300

    Ответ сотрудников 01

--- src/backend-mocking/handlers/employees/get.js
+++ src/backend-mocking/handlers/employees/get.js
@@ -2,24 +2,43 @@ import {http, HttpResponse} from "msw"
 
 import {getParamAsNumber, splitGetParamToNumberArray} from '@/backend-mocking/handlers/helpers.js'
 
+const perPage = 20
 
 export const createGetHandler = ({baseUrl, dbInstance})=>{
   return http.get(`${baseUrl}/employees`, ({request, params, cookies}) => {
-
-    // return HttpResponse.error()
     
     const url = new URL(request.url)
     const page = getParamAsNumber('page', url)
     const position_ids = splitGetParamToNumberArray('position_ids', url)
     
-    const allEmployees = dbInstance.employee.getAll()
-
-    // todo:: убрать хардкод
+    const selectedEmployees = dbInstance.employee.findMany({
+      orderBy: {
+        id: 'desc',
+      },
+      take: perPage,
+      skip: perPage * (page - 1),
+    })
+      .map(e=>{
+        let position = dbInstance.position.findFirst({
+          where: {
+            id: {
+              equals: e.position.id,
+            }
+          }
+        })
+        return {
+          id: e.id,
+          firstname: e.firstname,
+          lastname: e.lastname,
+          position: {
+            id: position.id
+          }
+        }
+      })
     return HttpResponse.json(
       {
-        data: [1,2,3]
+        data: selectedEmployees
       },
-      // { status: 401 }
       )
   })
 }
