a1216e7510e6e7c14b74b91271ad576ff1b05965
Taras Yemtsov <hardmagnett@yandex.ru>
2024-07-17 13:16:23 +0300

    todo

--- src/app/stores/position.ts
+++ src/app/stores/position.ts
@@ -4,8 +4,7 @@ import apiMain from '@/app/api/apiMain'
 
 import Position from '@/app/models/position/Position'
 import { useRepo } from 'pinia-orm'
-// todo:: возможно это придется сделать computed
-const positionRepo = useRepo(Position)
+const positionRepo = useRepo(Position)  // Если будет глючить - сделать его computed/getter. В примерах оно было computed/getter.
 
 import type {IPosition} from '@/app/types/apiEntities';
 
@@ -19,21 +18,6 @@ export const usePositionsStore = defineStore('counter', {
   },
   actions: {
     async fetchAllPositions() {
-
-      fetch(
-        'positions', {
-          method: 'get',
-        })
-        .then(function(res){
-          // console.log(res); console.log('^...res:')
-          return res.json();
-        })
-        .then(function(data){
-          console.log(data); console.log('^...data:')
-        })
-        .catch(function(err) {
-        });
-      
       let dataFromServer = await apiMain.fetch({
           method: 'get',
           url: 'positions'
