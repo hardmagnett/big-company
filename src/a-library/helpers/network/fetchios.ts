5e8862c40c0b1737a7e71a00ca9b28b5ba748369
Taras Yemtsov <hardmagnett@yandex.ru>
2024-07-17 12:44:29 +0300

    todo

--- src/a-library/helpers/network/fetchios.ts
+++ src/a-library/helpers/network/fetchios.ts
@@ -30,10 +30,7 @@ let convertObjectForURLSearchParams = (getParams: GetParams): Record<string, str
 type FetchParams = {
   url: string
   method: 'get' | 'post' | 'put' | 'delete',
-  // getParams?: Record<string, string|number>
-  // getParams?: Record<string, string>
   getParams?: GetParams
-
 }
 // todo:: сделать обработчики ошибок. Как в статье.
 class Fetchios {
@@ -43,14 +40,19 @@ class Fetchios {
   ){
     this.baseUrl = baseUrl
   }
+  private _prepareUrl({url, getParams}: {url: string, getParams: GetParams}){
+    let finalUrl = this.baseUrl + url
+    let convertedGetParams = convertObjectForURLSearchParams(getParams)
+    let searchParamsString = new URLSearchParams(convertedGetParams).toString()
+    if (searchParamsString) {
+      finalUrl = finalUrl + '?' + searchParamsString
+    }
+    return finalUrl
+  }
   fetch({url, method, getParams = {}}: FetchParams ){
     return new Promise((resolve, reject) => {
-      const finalWithBase = this.baseUrl + url
+      let finalUrl = this._prepareUrl({url, getParams})
 
-      let convertedGetParams = convertObjectForURLSearchParams(getParams)
-      // todo:: добавлять знак вопроса только если есть гет-параметры
-      const finalUrl = finalWithBase + '?' + new URLSearchParams(convertedGetParams).toString()
-      // console.log(finalUrl); console.log('^...finalUrl:') 
       fetch(
         finalUrl, {
           method: method,
