1d15fb8f91609de898c7112f4b3000f1a2995b5f
Taras Yemtsov <hardmagnett@yandex.ru>
2024-07-17 13:27:36 +0300

    todo

--- src/app/stores/employee.ts
+++ src/app/stores/employee.ts
@@ -2,8 +2,7 @@ import {defineStore} from "pinia";
 import apiMain from "@/app/api/apiMain";
 import type {IPosition} from "@/app/types/apiEntities";
 
-// todo:: разобраться с WTF здесь и в других местах
-export const useEmployeesStore = defineStore('WTF?', {
+export const useEmployeesStore = defineStore('employee', {
   actions: {
     async fetchPaginatedEmployees({page = 1}: {page?: number}) {
 
--- src/app/stores/position.ts
+++ src/app/stores/position.ts
@@ -9,7 +9,7 @@ const positionRepo = useRepo(Position)  // Если будет глючить - 
 import type {IPosition} from '@/app/types/apiEntities';
 
 
-export const usePositionsStore = defineStore('counter', {
+export const usePositionsStore = defineStore('positions', {
   getters: {
     allPositions: (state) => ()=>{
       const positions = positionRepo.get()
