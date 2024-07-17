ef3a1c3f69688a56eafa185340f1f11eb8156ea1
Taras Yemtsov <hardmagnett@yandex.ru>
2024-07-17 12:47:26 +0300

    todo

--- src/backend-mocking/handlers/employees/get.js
+++ src/backend-mocking/handlers/employees/get.js
@@ -1,27 +1,7 @@
 import {http, HttpResponse} from "msw"
 
-// todo:: повыносить эту срань в хелперы-файл
-const splitGetParamToArray = (paramName, urlInstance)=>{
-  return urlInstance.searchParams.has(paramName) 
-    ? urlInstance.searchParams.get(paramName).split(',') 
-    : []
-}
-const splitGetParamToNumberArray = (paramName, urlInstance)=>{
-  let result = splitGetParamToArray(paramName, urlInstance)
-  result = result.map(i=>Number(i))
-  return result
-}
+import {getParamAsNumber, splitGetParamToNumberArray} from '@/backend-mocking/handlers/helpers.js'
 
-const getParam = (paramName, urlInstance) =>{
-  return urlInstance.searchParams.has(paramName) ? urlInstance.searchParams.get(paramName) : null
-}
-const getParamAsNumber = (paramName, urlInstance) =>{
-  let result = getParam(paramName, urlInstance)
-  if (result) {
-    result = Number(result)
-  }
-  return result
-}
 
 export const createGetHandler = ({baseUrl, dbInstance})=>{
   return http.get(`${baseUrl}/employees`, ({request, params, cookies}) => {
